59f7f7291e2a63be621ca1a41f83f8ffcf4955d7
Lionel Mason <capitalcookhouse@gmail.com>
2024-12-02 07:56:35 -0500

    completes roll_call_dwarves function

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,5 +1,8 @@
-def roll_call_dwarves():
+def roll_call_dwarves(names):
     pass
+    for index, name in enumerate(names, start=1):
+        print(f"{index}. {name}")
+
 
 def summon_captain_planet():
     pass
