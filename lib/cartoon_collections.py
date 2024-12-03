fad05405fc55b11fdc6c075f0f705c2225aaabfb
Lionel Mason <capitalcookhouse@gmail.com>
2024-12-03 08:58:58 -0500

    completes cartoon-collections-lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,3 +1,7 @@
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+calls = ["puff", "go", "two"]
+snacks = ["crackers", "gouda", "thyme"]
+
 def roll_call_dwarves(names):
     pass
     for index, name in enumerate(names, start=1):
@@ -7,11 +11,21 @@ def roll_call_dwarves(names):
 def summon_captain_planet(planeteer_calls):
     pass
     return [call.capitalize() + "!" for call in planeteer_calls]
+    # result = summon_captain_planet(planeteer_calls)
+    # print(result)
 
 
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
     pass
+    return any(len(call) > 4 for call in calls)
+   
 
-def find_the_cheese():
+def find_the_cheese(strings):
     pass
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in strings:
+        if item in cheeses:
+            return item
+    return None
+
